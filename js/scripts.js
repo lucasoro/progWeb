2a4b8c4db5eea32097169bc01b11d728280ea4b9
Lucas Oro <lucasoro97@gmail.com>
2020-11-18 19:00:05 -0300

    criação da página de contato e implementação do mapa

--- js/scripts.js
+++ js/scripts.js
@@ -96,4 +96,36 @@ $(document).ready(function() {
             alert("Realize o login primeiro!");
         }
     })
+
+    function initMap() {
+        // The map, centered at Curitiba
+        const map = new google.maps.Map(document.getElementById("map"), {
+            zoom: 13,
+            center: { lat: -25.429, lng: -49.267 },
+        });
+
+        addMarker({
+            coords: { lat: -25.439, lng: -49.237 },
+            title: ' </h1> loja 1 </h1>'
+
+        });
+
+        addMarker({
+            coords: { lat: -25.499, lng: -49.227 },
+            title: '<h1> Loja 2 </h1>'
+
+        });
+        addMarker({
+            coords: { lat: -25.459, lng: -49.257 },
+            title: '<h1> Loja 3 </h1>'
+        });
+
+        function addMarker(props) {
+            var marker = new google.maps.Marker({
+                position: props.coords,
+                map: map,
+            });
+        }
+    }
+    initMap()
 })
